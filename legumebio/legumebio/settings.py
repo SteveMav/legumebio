6209718df7eee568980cfac3b796749f4572d0c4
steve mavuela <stevemavuela@gmail.com>
2024-09-08 19:59:26 +0100

    teset

--- legumebio/legumebio/settings.py
+++ legumebio/legumebio/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'vegetable_shop',
     'accounts',
-     'django_celery_beat',
 
 ]
 
@@ -146,9 +145,10 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
